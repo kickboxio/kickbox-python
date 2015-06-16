860aab2244247738fdf29b3711ebe0c9f8fbd49b
Chaitanya Surapaneni <chaitanyakrishnas@gmail.com>
2015-06-16 10:28:30 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='kickbox',
-    version='2.0.0',
+    version='2.0.2',
     description='Official kickbox API library client for python',
     author='Chaitanya Surapaneni',
     author_email='chaitanya.surapaneni@kickbox.io',
